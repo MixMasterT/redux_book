d4a67bac9eefa4bb5ab8b0b92f62101d3152266a
mixmastert <torahlight@gmail.com>
2017-04-17 23:25:04 -0700

    modify app.js

--- app/app.js
+++ app/app.js
@@ -2,18 +2,9 @@ import 'normalize.css/normalize.css';
 import 'assets/stylesheets/main.css';
 import { createStore } from 'redux';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1;
-  }
+const reducer = (state, action) => state;
 
-  return state;
-};
-
-const initialState = 0;
-
-const store = createStore(reducer, initialState);
+const store = createStore(reducer);
 
 store.subscribe(() => document.getElementById('counter').innerText = store.getState());
 
