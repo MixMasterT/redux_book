1ce73fe1c900718d5c55540a6c127ad7a0168a1c
mixmastert <torahlight@gmail.com>
2017-04-17 23:29:28 -0700

    put store on window

--- app/app.js
+++ app/app.js
@@ -21,8 +21,6 @@ const reducer = (state, action) => state;
 
 const store = createStore(reducer, initialState);
 
-store.subscribe(() => document.getElementById('counter').innerText = store.getState());
-
-setInterval(() => store.dispatch({ type: 'INC' }), 500);
+window.store = store;
 
 console.log("Redux started");
